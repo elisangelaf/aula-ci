c4e445608111b2cafffb976c098d986d296c8262
elisangelaf <116315155+elisangelaf@users.noreply.github.com>
2024-04-15 20:35:19 -0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,10 +1,9 @@
 package main
 import "testing"
-
 func TestAdd(t *testing.T) {
     result := Add(2, 3)
     expected := 5
-    if resulted != expected {
-      t.Errorf("Add(2, 3) = "%d; want %d", result, expected)
+    if result != expected {
+        t.Errorf("Add(2, 3) = %d; want %d", result, expected)
     }
-  }
+}
